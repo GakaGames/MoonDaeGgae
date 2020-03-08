69e3d12405d6fd0020fbb417a7b20d6f78ab30da
GakaTV <gakatv@hotmail.com>
2020-03-08 21:40:58 +0900

    Add resources.js

---
+++ js/resources.js
@@ -0,0 +1,62 @@
+game.resources = [
+    {
+        "name": "texture",
+        "type": "json",
+        "src": "data/img/texture.json"
+    },
+    {
+        "name": "texture",
+        "type": "image",
+        "src": "data/img/texture.png"
+    },
+    {
+        "name": "moonminem",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "red_sun_in_the_sky",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "argh",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "dingdong",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "PressStart2P",
+        "type": "image",
+        "src": "data/fnt/PressStart2P.png"
+    },
+    {
+        "name": "PressStart2P",
+        "type": "binary",
+        "src": "data/fnt/PressStart2P.fnt"
+    },
+    {
+        "name": "moonrona",
+        "type": "image",
+        "src": "data/img/gui/moonrona.png"
+    },
+    {
+        "name": "title_screen",
+        "type": "image",
+        "src": "data/img/gui/title_screen.png"
+    },
+    {
+        "name": "texture",
+        "type": "image",
+        "src": "data/img/texture.png"
+    },
+    {
+        "name": "texture",
+        "type": "json",
+        "src": "data/img/texture.json"
+    }
+];
